2feff7a8c3c82d70a713f4ccb405053d9bd8038a
yashmitb <yashmitb07@gmail.com>
2022-10-16 19:09:36 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDO0sorP8bfkJ0Ha5UU8ahzxOXSvEJ71D8",
+  authDomain: "spectogram-365801.firebaseapp.com",
+  projectId: "spectogram-365801",
+  storageBucket: "spectogram-365801.appspot.com",
+  messagingSenderId: "671143795400",
+  appId: "1:671143795400:web:31dca9fbd75798503f23fd",
+};
